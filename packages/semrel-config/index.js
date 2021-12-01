2e950b5bf3b8c41531c5e58c773794b66b57d20f
Diego Weinmann <diegoweinmann@gmail.com>
2021-12-01 01:22:12 -0300

    build: update semrel-config

--- packages/semrel-config/index.js
+++ packages/semrel-config/index.js
@@ -3,27 +3,19 @@ const branches = ['main'];
 const plugins = [
   '@semantic-release/commit-analyzer',
   '@semantic-release/release-notes-generator',
-	'semrel-plugin',
+	'./node_modules/semrel-plugin',
   [
     '@semantic-release/npm',
     {
       npmPublish: false,
     },
-  ],
-  [
-    '@semantic-release/github',
-    {
-      successComment: false,
-      failComment: false,
-    },
-  ],
+  ]
 ];
 
 module.exports = {
-	extends: 'semantic-release-monorepo',
   branches,
   plugins,
 	dryRun: true,
-	ci: true,
-	debug: process.env.DEBUG_SEMATIC_RELEASE || true
+	ci: false,
+	debug: true
 };
